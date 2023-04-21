de5399308b9a233a18a39415915e520b73354628
eoruadl <dg7202@gmail.com>
2023-04-21 17:54:45 +0900

    feat: event, user 기능 구현 및 mongoDB 연동

--- models/users.py
+++ models/users.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
 from models.events import Event
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
     events: Optional[List[Event]]
+
+    class Settings:
+        name = "users"
 
     class Config:
         schema_extra = {
@@ -20,11 +25,11 @@ class UserSignIn(BaseModel):
     email: EmailStr
     password: str
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "password": "strong!!!",
-                "events": [],
-            }
-        }
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "email": "fastapi@packt.com",
+    #             "password": "strong!!!",
+    #             "events": [],
+    #         }
+    #     }
